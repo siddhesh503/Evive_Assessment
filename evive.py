f733c73b1442439a74c83286eef72f9c48b39642
siddhesh <chavansiddhesh785@gmail.com>
2018-08-09 18:42:05 -0500

    modularized the code

--- evive.py
+++ evive.py
@@ -5,67 +5,21 @@ from collections import Counter
 from utils import utils
 
 starting_time = calendar.timegm(time.gmtime())
-########################################getting movie list#####################################################33
-page_number = 1
-response_dict = utils.get_media_ids(page_number,"movie",["primary_release_date.gte","primary_release_date.lte"])
-page_number += 1
-movie_id_list = response_dict['id_set']
-X_RateLimit_Reset = response_dict['X_RateLimit_Reset']
-X_RateLimit_Remaining = response_dict['X_RateLimit_Remaining']
-total_pages = response_dict['total_pages']
-
-movie_id_list = utils.get_total_media_ids(["primary_release_date.gte","primary_release_date.lte"],"movie",page_number,total_pages,X_RateLimit_Remaining,X_RateLimit_Reset,movie_id_list)
-
+########################################getting movie list#####################################################
+movie_id_list = utils.get_total_media_ids(["primary_release_date.gte","primary_release_date.lte"],"movie")
+movie_id_list = list(movie_id_list)
 print(len(movie_id_list))
-movie_id_list = list(set(movie_id_list))
-print(len(movie_id_list))
-
 #############################getting cast for movies##############################################
-
-response_dict = utils.get_cast_ids("movie",movie_id_list[0])
-cast_id_list = response_dict["id_set"]
-print(cast_id_list)
-X_RateLimit_Reset = response_dict["X_RateLimit_Reset"] 
-X_RateLimit_Remaining = response_dict["X_RateLimit_Remaining"] 
-counter = 1
-
-cast_id_list = utils.get_total_cast_ids("movie",counter,movie_id_list,X_RateLimit_Remaining,X_RateLimit_Reset,cast_id_list)
-
-cast_id_list = list(set(cast_id_list))
+cast_id_list = utils.get_total_cast_ids("movie",movie_id_list)
+cast_id_list = list(cast_id_list)
 print(len(cast_id_list))
-
 ####################################################getting tv show list######################################
-page_number = 1
-response_dict = utils.get_media_ids(page_number,"tv",["air_date.gte","air_date.lte"])
-page_number += 1
-tv_id_list = response_dict['id_set']
-X_RateLimit_Reset = response_dict['X_RateLimit_Reset']
-X_RateLimit_Remaining = response_dict['X_RateLimit_Remaining']
-total_pages = response_dict['total_pages']
-
-
-tv_id_list = utils.get_total_media_ids(["air_date.gte","air_date.lte"],"tv",page_number,total_pages,X_RateLimit_Remaining,X_RateLimit_Reset,tv_id_list)
-
-
-
+tv_id_list = utils.get_total_media_ids(["air_date.gte","air_date.lte"],"tv")
+tv_id_list = list(tv_id_list)
 print(len(tv_id_list))
-tv_id_list = list(set(tv_id_list))
-print(len(tv_id_list))
-
 ######################################################getting tv cast######################################
-response_dict = utils.get_cast_ids("tv",tv_id_list[0])
-cast_id_list_tv = response_dict["id_set"]
-print(cast_id_list_tv)
-X_RateLimit_Reset = response_dict["X_RateLimit_Reset"] 
-X_RateLimit_Remaining = response_dict["X_RateLimit_Remaining"] 
-counter = 1
-
-
-cast_id_list_tv = utils.get_total_cast_ids("tv",counter,tv_id_list,X_RateLimit_Remaining,X_RateLimit_Reset,cast_id_list_tv)
-
-cast_id_list_tv = list(set(cast_id_list_tv))
-
-
+cast_id_list_tv = utils.get_total_cast_ids("tv",tv_id_list)
+cast_id_list_tv = list(cast_id_list_tv)
 ###########################################showing results!!#################################################3
 print('------------------------------ count is -----------------------------------')
 print(len(set(cast_id_list_tv).intersection(set(cast_id_list))))
--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,21 @@ import calendar;
 import time;
 from collections import Counter
 
+cached_headers = {
+	'X-RateLimit-Reset' : calendar.timegm(time.gmtime()) + 9.5,
+	'X-RateLimit-Remaining' : 40
+}
 ######################################## utility functions##############################################
+"""
+	This is for putting dynamic delay
+"""
+def dynamic_delay(X_RateLimit_Reset):
+	print('------------------inside delay---------------')
+	print(X_RateLimit_Reset)
+	print(calendar.timegm(time.gmtime()))
+	time.sleep(X_RateLimit_Reset - calendar.timegm(time.gmtime())+1) #adding 1 seconds is just to add a buffer just to be safe
+	print(calendar.timegm(time.gmtime()))
+
 """
 	This is a function for getting a media(tv/movie) ids on a single page
 """
@@ -14,21 +28,25 @@ def get_media_ids(page_number,media_type,query_params):
 		response = requests.get(url, headers={'User-Agent':'my agent'})
 		response_json =response.json()
 		results = response_json['results']
+		total_pages = response_json['total_pages']
 		print(response.headers)
 		print(page_number)
+		print(url)
 		id_set = set()
 		for result_dict in results:
 			try:
 				id_set.add(result_dict["id"])            ######sometimes results have null objects
 			except Exception as e:
 				print(e)
-		X_RateLimit_Reset = int(response.headers['X-RateLimit-Reset'])
-		X_RateLimit_Remaining = int(response.headers['X-RateLimit-Remaining'])
-		return ({'id_set':id_set,'X_RateLimit_Reset':X_RateLimit_Reset,'X_RateLimit_Remaining':X_RateLimit_Remaining,'total_pages':total_pages})
+		cached_headers['X-RateLimit-Reset'] = int(response.headers['X-RateLimit-Reset'])
+		cached_headers['X-RateLimit-Remaining'] = int(response.headers['X-RateLimit-Remaining'])
+		return ({'id_set':id_set,'total_pages':total_pages})
 	except Exception as e:
 			print(e)
+			cached_headers['X-RateLimit-Reset'] = calendar.timegm(time.gmtime())
+			cached_headers['X-RateLimit-Remaining'] = 1
 			default_set = set()
-			return ({'id_set':default_set,'X_RateLimit_Reset': 1,'X_RateLimit_Remaining':calendar.timegm(time.gmtime())})    ###default dict to keep program running
+			return ({'id_set':default_set,'total_pages':0})    ###default dict to keep program running
 
 """
 	This is a function for getting cast ids associated with a single media id
@@ -41,24 +59,34 @@ def get_cast_ids(media_type,media_id):
 		response_json =response.json()
 		cast_list = response_json['cast']
 		id_set = set()
+		print(url)
 		for cast_dict in cast_list:
 			try:
 				id_set.add(cast_dict["id"])            ######sometimes results have null objects
 			except Exception as e:
 				print(e)
-		X_RateLimit_Reset = int(response.headers['X-RateLimit-Reset'])
-		X_RateLimit_Remaining = int(response.headers['X-RateLimit-Remaining'])
-		return ({'id_set':id_set,'X_RateLimit_Reset':X_RateLimit_Reset,'X_RateLimit_Remaining':X_RateLimit_Remaining})
+		cached_headers['X-RateLimit-Reset'] = int(response.headers['X-RateLimit-Reset'])
+		cached_headers['X-RateLimit-Remaining'] = int(response.headers['X-RateLimit-Remaining'])
+		return ({'id_set':id_set})
 	except Exception as e:
 		print(e)
+		cached_headers['X-RateLimit-Reset'] = calendar.timegm(time.gmtime())
+		cached_headers['X-RateLimit-Remaining'] = 1
 		default_set = set()
-		return ({'id_set':default_set,'X_RateLimit_Reset': 1,'X_RateLimit_Remaining':calendar.timegm(time.gmtime())})    ###default dict to keep program running
+		return ({'id_set':default_set})    ###default dict to keep program running
 
 
 """
 	This is a function for getting media ids on all pages
 """
-def get_total_media_ids(query_params,media_type,page_number,total_pages,X_RateLimit_Remaining,X_RateLimit_Reset,media_id_list):
+def get_total_media_ids(query_params,media_type):
+	page_number = 1
+	response_dict = get_media_ids(page_number,media_type,query_params)
+	page_number += 1
+	media_id_list = response_dict['id_set']
+	X_RateLimit_Reset = cached_headers['X-RateLimit-Reset']
+	X_RateLimit_Remaining = cached_headers['X-RateLimit-Remaining']
+	total_pages = response_dict['total_pages']
 	"""
 		TMDB allows to access only 1000 pages
 		here is the reference on their official website:
@@ -68,10 +96,7 @@ def get_total_media_ids(query_params,media_type,page_number,total_pages,X_RateLi
 		if X_RateLimit_Remaining == 0:
 			try:
 				print('------------------inside delay---------------')
-				print(X_RateLimit_Reset)
-				print(calendar.timegm(time.gmtime()))
-				time.sleep(X_RateLimit_Reset - calendar.timegm(time.gmtime())+1) #adding 1 seconds is just to add a buffer in case of any 
-				print(calendar.timegm(time.gmtime()))
+				dynamic_delay(X_RateLimit_Reset)
 				X_RateLimit_Remaining = 1 # assigning any non zero value to make the for loop go forward and to not enter this if block again unless necessary
 			except Exception as e:
 				print(e)
@@ -81,23 +106,25 @@ def get_total_media_ids(query_params,media_type,page_number,total_pages,X_RateLi
 			page_number += 1
 			id_set = response_dict['id_set']
 			media_id_list = media_id_list.union(id_set)
-			X_RateLimit_Reset = response_dict['X_RateLimit_Reset']
-			X_RateLimit_Remaining = response_dict['X_RateLimit_Remaining']
+			X_RateLimit_Reset = cached_headers['X-RateLimit-Reset']
+			X_RateLimit_Remaining = cached_headers['X-RateLimit-Remaining']
 
 	return media_id_list
 
 """
 	This is a function for getting cast ids associated with all the media ids
 """
-def get_total_cast_ids(media_type,counter,media_id_list,X_RateLimit_Remaining,X_RateLimit_Reset,cast_id_list):
+def get_total_cast_ids(media_type,media_id_list):
+	response_dict = get_cast_ids(media_type,media_id_list[0])
+	cast_id_list = response_dict["id_set"]
+	print(cast_id_list)
+	X_RateLimit_Reset = cached_headers['X-RateLimit-Reset']
+	X_RateLimit_Remaining = cached_headers['X-RateLimit-Remaining']
+	counter = 1
 	while counter < len(media_id_list) :
 		if X_RateLimit_Remaining == 0:
 			try:
-				print('------------------inside delay---------------')
-				print(X_RateLimit_Reset)
-				print(calendar.timegm(time.gmtime()))
-				time.sleep(X_RateLimit_Reset - calendar.timegm(time.gmtime())+1) #adding 1 second is just to add a buffer in case of any 
-				print(calendar.timegm(time.gmtime()))
+				dynamic_delay(X_RateLimit_Reset)
 				X_RateLimit_Remaining = 1 # assigning any non zero value to make the for loop go forward and to not enter this if block again unless necessary
 			except Exception as e:
 				print(e)
@@ -106,8 +133,8 @@ def get_total_cast_ids(media_type,counter,media_id_list,X_RateLimit_Remaining,X_
 			response_dict = get_cast_ids(media_type,media_id_list[counter])
 			id_set = response_dict["id_set"]
 			cast_id_list = cast_id_list.union(id_set)
-			X_RateLimit_Reset = response_dict["X_RateLimit_Reset"] 
-			X_RateLimit_Remaining = response_dict["X_RateLimit_Remaining"]
+			X_RateLimit_Reset = cached_headers['X-RateLimit-Reset']
+			X_RateLimit_Remaining = cached_headers['X-RateLimit-Remaining']
 			print(counter) 
 			counter+=1
 
