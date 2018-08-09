602493ec4761a7eaa274fb2c1629147b8d332cbe
siddhesh <chavansiddhesh785@gmail.com>
2018-08-09 15:14:55 -0500

    modularized some more. removed code duplication

--- evive.py
+++ evive.py
@@ -14,31 +14,7 @@ X_RateLimit_Reset = response_dict['X_RateLimit_Reset']
 X_RateLimit_Remaining = response_dict['X_RateLimit_Remaining']
 total_pages = response_dict['total_pages']
 
-"""
-TMDB allows to access only 1000 pages
-here is the reference on their official website:
-https://www.themoviedb.org/talk/590ca6eac3a36865190008be?language=en
-"""
-
-while page_number <= total_pages and page_number <=1000 :
-	if X_RateLimit_Remaining == 0:
-		try:
-			print('------------------inside delay---------------')
-			print(X_RateLimit_Reset)
-			print(calendar.timegm(time.gmtime()))
-			time.sleep(X_RateLimit_Reset - calendar.timegm(time.gmtime())+1) #adding 0.5 seconds is just to add a buffer in case of any 
-			print(calendar.timegm(time.gmtime()))
-			X_RateLimit_Remaining = 1 # assigning any non zero value to make the for loop go forward and to not enter this if block again unless necessary
-		except Exception as e:
-			print(e)
-			page_number += 1
-	else:
-		response_dict = utils.get_media_ids(page_number,"movie",["primary_release_date.gte","primary_release_date.lte"])
-		page_number += 1
-		id_set = response_dict['id_set']
-		movie_id_list = movie_id_list.union(id_set)
-		X_RateLimit_Reset = response_dict['X_RateLimit_Reset']
-		X_RateLimit_Remaining = response_dict['X_RateLimit_Remaining']
+movie_id_list = utils.get_total_media_ids(["primary_release_date.gte","primary_release_date.lte"],"movie",page_number,total_pages,X_RateLimit_Remaining,X_RateLimit_Reset,movie_id_list)
 
 print(len(movie_id_list))
 movie_id_list = list(set(movie_id_list))
@@ -53,26 +29,7 @@ X_RateLimit_Reset = response_dict["X_RateLimit_Reset"]
 X_RateLimit_Remaining = response_dict["X_RateLimit_Remaining"] 
 counter = 1
 
-while counter < len(movie_id_list) :
-	if X_RateLimit_Remaining == 0:
-		try:
-			print('------------------inside delay---------------')
-			print(X_RateLimit_Reset)
-			print(calendar.timegm(time.gmtime()))
-			time.sleep(X_RateLimit_Reset - calendar.timegm(time.gmtime())+1) #adding 1 second is just to add a buffer in case of any 
-			print(calendar.timegm(time.gmtime()))
-			X_RateLimit_Remaining = 1 # assigning any non zero value to make the for loop go forward and to not enter this if block again unless necessary
-		except Exception as e:
-			print(e)
-			counter+=1
-	else:
-		response_dict = utils.get_cast_ids("movie",movie_id_list[counter])
-		id_set = response_dict["id_set"]
-		cast_id_list = cast_id_list.union(id_set)
-		X_RateLimit_Reset = response_dict["X_RateLimit_Reset"] 
-		X_RateLimit_Remaining = response_dict["X_RateLimit_Remaining"]
-		print(counter) 
-		counter+=1
+cast_id_list = utils.get_total_cast_ids("movie",counter,movie_id_list,X_RateLimit_Remaining,X_RateLimit_Reset,cast_id_list)
 
 cast_id_list = list(set(cast_id_list))
 print(len(cast_id_list))
@@ -86,31 +43,9 @@ X_RateLimit_Reset = response_dict['X_RateLimit_Reset']
 X_RateLimit_Remaining = response_dict['X_RateLimit_Remaining']
 total_pages = response_dict['total_pages']
 
-"""
-TMDB allows to access only 1000 pages
-here is the reference on their official website:
-https://www.themoviedb.org/talk/590ca6eac3a36865190008be?language=en
-"""
 
-while page_number <= total_pages and page_number <=1000 :
-	if X_RateLimit_Remaining == 0:
-		try:
-			print('------------------inside delay---------------')
-			print(X_RateLimit_Reset)
-			print(calendar.timegm(time.gmtime()))
-			time.sleep(X_RateLimit_Reset - calendar.timegm(time.gmtime())+1) #adding 0.5 seconds is just to add a buffer in case of any 
-			print(calendar.timegm(time.gmtime()))
-			X_RateLimit_Remaining = 1 # assigning any non zero value to make the for loop go forward and to not enter this if block again unless necessary
-		except Exception as e:
-			print(e)
-			page_number += 1
-	else:
-		response_dict = utils.get_media_ids(page_number,"tv",["air_date.gte","air_date.lte"])
-		page_number += 1
-		id_set = response_dict['id_set']
-		tv_id_list = tv_id_list.union(id_set)
-		X_RateLimit_Reset = response_dict['X_RateLimit_Reset']
-		X_RateLimit_Remaining = response_dict['X_RateLimit_Remaining']
+tv_id_list = utils.get_total_media_ids(["air_date.gte","air_date.lte"],"tv",page_number,total_pages,X_RateLimit_Remaining,X_RateLimit_Reset,tv_id_list)
+
 
 
 print(len(tv_id_list))
@@ -125,26 +60,9 @@ X_RateLimit_Reset = response_dict["X_RateLimit_Reset"]
 X_RateLimit_Remaining = response_dict["X_RateLimit_Remaining"] 
 counter = 1
 
-while counter < len(tv_id_list) :
-	if X_RateLimit_Remaining == 0:
-		try:
-			print('------------------inside delay---------------')
-			print(X_RateLimit_Reset)
-			print(calendar.timegm(time.gmtime()))
-			time.sleep(X_RateLimit_Reset - calendar.timegm(time.gmtime())+1) #adding 1 second is just to add a buffer in case of any 
-			print(calendar.timegm(time.gmtime()))
-			X_RateLimit_Remaining = 1 # assigning any non zero value to make the for loop go forward and to not enter this if block again unless necessary
-		except Exception as e:
-			print(e)
-			counter+=1
-	else:
-		response_dict = utils.get_cast_ids("tv",tv_id_list[counter])
-		id_set = response_dict["id_set"]
-		cast_id_list_tv = cast_id_list_tv.union(id_set)
-		X_RateLimit_Reset = response_dict["X_RateLimit_Reset"] 
-		X_RateLimit_Remaining = response_dict["X_RateLimit_Remaining"]
-		print(counter) 
-		counter+=1
+
+cast_id_list_tv = utils.get_total_cast_ids("tv",counter,tv_id_list,X_RateLimit_Remaining,X_RateLimit_Reset,cast_id_list_tv)
+
 
 
 cast_id_list_tv = list(set(cast_id_list_tv))
--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,9 @@ import time;
 from collections import Counter
 
 ######################################## utility functions##############################################
+"""
+	This is a function for getting a media(tv/movie) ids on a single page
+"""
 def get_media_ids(page_number,media_type,query_params):
 	try:
 		base_url = "http://api.themoviedb.org/3/discover/{}?{}=2017-12-01&{}=2017-12-31&api_key=606aaffd7ca10f0b80804a1f0674e4e1"
@@ -28,6 +31,9 @@ def get_media_ids(page_number,media_type,query_params):
 			default_set = set()
 			return ({'id_set':default_set,'X_RateLimit_Reset': 1,'X_RateLimit_Remaining':calendar.timegm(time.gmtime())})    ###default dict to keep program running
 
+"""
+	This is a function for getting cast ids associated with a single media id
+"""
 def get_cast_ids(media_type,media_id):
 	try:
 		base_url = "https://api.themoviedb.org/3/{}/{}/credits?api_key=606aaffd7ca10f0b80804a1f0674e4e1"
@@ -48,3 +54,62 @@ def get_cast_ids(media_type,media_id):
 		print(e)
 		default_set = set()
 		return ({'id_set':default_set,'X_RateLimit_Reset': 1,'X_RateLimit_Remaining':calendar.timegm(time.gmtime())})    ###default dict to keep program running
+
+
+"""
+	This is a function for getting media ids on all pages
+"""
+def get_total_media_ids(query_params,media_type,page_number,total_pages,X_RateLimit_Remaining,X_RateLimit_Reset,media_id_list):
+	"""
+		TMDB allows to access only 1000 pages
+		here is the reference on their official website:
+		https://www.themoviedb.org/talk/590ca6eac3a36865190008be?language=en
+	"""
+	while page_number <= total_pages and page_number <=1000 :
+		if X_RateLimit_Remaining == 0:
+			try:
+				print('------------------inside delay---------------')
+				print(X_RateLimit_Reset)
+				print(calendar.timegm(time.gmtime()))
+				time.sleep(X_RateLimit_Reset - calendar.timegm(time.gmtime())+1) #adding 1 seconds is just to add a buffer in case of any 
+				print(calendar.timegm(time.gmtime()))
+				X_RateLimit_Remaining = 1 # assigning any non zero value to make the for loop go forward and to not enter this if block again unless necessary
+			except Exception as e:
+				print(e)
+				page_number += 1
+		else:
+			response_dict = get_media_ids(page_number,media_type,query_params)
+			page_number += 1
+			id_set = response_dict['id_set']
+			media_id_list = media_id_list.union(id_set)
+			X_RateLimit_Reset = response_dict['X_RateLimit_Reset']
+			X_RateLimit_Remaining = response_dict['X_RateLimit_Remaining']
+
+	return media_id_list
+
+"""
+	This is a function for getting cast ids associated with all the media ids
+"""
+def get_total_cast_ids(media_type,counter,media_id_list,X_RateLimit_Remaining,X_RateLimit_Reset,cast_id_list):
+	while counter < len(media_id_list) :
+		if X_RateLimit_Remaining == 0:
+			try:
+				print('------------------inside delay---------------')
+				print(X_RateLimit_Reset)
+				print(calendar.timegm(time.gmtime()))
+				time.sleep(X_RateLimit_Reset - calendar.timegm(time.gmtime())+1) #adding 1 second is just to add a buffer in case of any 
+				print(calendar.timegm(time.gmtime()))
+				X_RateLimit_Remaining = 1 # assigning any non zero value to make the for loop go forward and to not enter this if block again unless necessary
+			except Exception as e:
+				print(e)
+				counter+=1
+		else:
+			response_dict = get_cast_ids(media_type,media_id_list[counter])
+			id_set = response_dict["id_set"]
+			cast_id_list = cast_id_list.union(id_set)
+			X_RateLimit_Reset = response_dict["X_RateLimit_Reset"] 
+			X_RateLimit_Remaining = response_dict["X_RateLimit_Remaining"]
+			print(counter) 
+			counter+=1
+
+	return cast_id_list
